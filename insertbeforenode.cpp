408422ee0efb9e3ae6218e08fe71b1ea26256e73
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-07-21 10:07:32 +0530

    Add files via upload

---
+++ insertbeforenode.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node * next;
+    node* back;
+    public:
+    node(int data1,node* next1,node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+
+};
+void print(node*head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+node* converttodll(vector<int>&arr){
+    node* head=new node(arr[0]);
+    node* prev=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+node* insertbeforhead(node * head,int val){
+    node * newhead=new node(val,head,nullptr);
+    head->back=newhead;
+    return newhead;
+}
+void insertbeforenode(node* Node,int val){
+    node* prev=Node->back;
+    node* newnode=new node(val,Node,prev);
+    prev->next=newnode;
+    Node->back=newnode;
+}
+    int main(){
+    vector<int>arr={12,5,8,7};
+    node*head=converttodll(arr);
+    insertbeforenode(head->next->next,1000);
+    print(head);
+    return 0;
+}
