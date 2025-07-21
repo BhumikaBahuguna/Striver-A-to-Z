8bdedeb16c503c1b6082e94740b6d06f889220a4
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-07-21 09:33:45 +0530

    Add files via upload

---
+++ insertbeforehead.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node * next;
+    node* back;
+    public:
+    node(int data1,node* next1,node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+
+};
+void print(node*head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+node* converttodll(vector<int>&arr){
+    node* head=new node(arr[0]);
+    node* prev=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+node* insertbeforhead(node * head,int val){
+    node * newhead=new node(val,head,nullptr);
+    head->back=newhead;
+    return newhead;
+}
+int main(){
+    vector<int>arr={12,5,8,7};
+    node*head=converttodll(arr);
+    head=insertbeforhead(head,10);
+    print(head);
+    return 0;
+}
